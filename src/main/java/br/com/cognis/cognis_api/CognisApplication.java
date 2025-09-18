50988841065c5987d1b8afc5492d926c38480981
Felipe De Paula <163021867+felipe-de-paula-dev@users.noreply.github.com>
2025-09-18 08:51:00 -0300

    Adicionando Arquivos De Projeto Spring

---
+++ src/main/java/br/com/cognis/cognis_api/CognisApplication.java
@@ -0,0 +1,13 @@
+package br.com.cognis.cognis_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cognis/cognis_api/CognisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cognis.cognis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
